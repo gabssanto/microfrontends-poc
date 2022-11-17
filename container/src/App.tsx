ece00f820a3258fba0a43d77bfac6d5a8c6b1f2a
gsanto <espiritosanto.gabriel@gmail.com>
2022-11-17 20:31:28 -0300

    first version of microfrontends project working

---
+++ container/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Counter } from "counter/Counter";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <h1>Container App</h1>
+    <Counter header="joao" /> // Micro frontend app
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
